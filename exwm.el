bd2b2dc8346c11d1c5073bb69f3970614a8c6f2c
Ilya Ostapyshyn <ilya.ostapyshyn@gmail.com>
2020-05-09 21:09:40 +0300

    exwm

---
+++ exwm.el
@@ -0,0 +1,31 @@
+(use-package exwm
+  :ensure t
+  :demand t)
+
+(setq exwm-workspace-number 4)
+;; Make class name the buffer name
+(add-hook 'exwm-update-class-hook
+          (lambda ()
+            (exwm-workspace-rename-buffer exwm-class-name)))
+;; Global keybindings.
+(unless (get 'exwm-input-global-keys 'saved-value)
+  (setq exwm-input-global-keys
+        `(
+          ;; 's-r': Reset (to line-mode).
+          ([?\s-r] . exwm-reset)
+          ;; 's-w': Switch workspace.
+          ([?\s-w] . exwm-workspace-switch)
+          ;; 's-&': Launch application.
+          ([?\s-&] . (lambda (command)
+                       (interactive (list (read-shell-command "$ ")))
+                       (start-process-shell-command command nil command)))
+          ;; 's-N': Switch to certain workspace.
+          ,@(mapcar (lambda (i)
+                      `(,(kbd (format "s-%d" i)) .
+                        (lambda ()
+                          (interactive)
+                          (exwm-workspace-switch-create ,i))))
+                    (number-sequence 0 4)))))
+;; Enable EXWM
+(exwm-enable)
+(exwm-init)
