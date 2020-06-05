fba9ba3bf0826a3149c20b81406af4a6c96124ca
Ilya Ostapyshyn <ilya.ostapyshyn@gmail.com>
2020-06-05 11:31:51 +0300

    use rather kbd

--- init.el
+++ init.el
@@ -97,7 +97,7 @@
 (recentf-mode 1)
 (setq recentf-max-menu-items 25)
 (setq recentf-max-saved-items 25)
-(global-set-key "\C-x\ \C-r" 'recentf-open-files)
+(global-set-key (kbd "C-x C-r") 'recentf-open-files)
 
 ;; Calc
 (with-eval-after-load 'calc
