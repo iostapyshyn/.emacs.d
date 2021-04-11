dcaabb30b60e13017c09e280f35d66cb690b05bb
Illia Ostapyshyn <ilya.ostapyshyn@gmail.com>
2021-04-11 13:27:52 +0200

    make recentf bigger

--- init.el
+++ init.el
@@ -94,8 +94,7 @@
 
 ;; Recentf
 (recentf-mode 1)
-(setq recentf-max-menu-items 25)
-(setq recentf-max-saved-items 25)
+(setq recentf-max-saved-items 50)
 (setq recentf-exclude '("^/var/folders\\.*"
                         "COMMIT_EDITMSG\\'"
                         ".*-autoloads\\.el\\'"
