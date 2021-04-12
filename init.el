b3c4fcf78b74cad8577231d0bee093b99939dd33
Illia Ostapyshyn <ilya.ostapyshyn@gmail.com>
2021-04-12 15:23:12 +0200

    wrap ligatures

--- init.el
+++ init.el
@@ -124,7 +124,9 @@
 (when (or (eq window-system 'ns) (eq window-system 'mac))
   (setq mac-command-modifier 'super
         mac-option-modifier 'meta
-        mac-right-option-modifier nil)
+        mac-right-option-modifier nil))
+
+(when (eq window-system 'mac)
   (mac-auto-operator-composition-mode 1))
 
 (add-to-list 'auto-mode-alist '("\\.ino$" . c++-mode))
