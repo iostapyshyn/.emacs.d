8e10e601ad069afeea0e028473bb282ae646953f
Illia Ostapyshyn <ilya.ostapyshyn@gmail.com>
2021-04-16 12:52:00 +0200

    narrow but bigger font

--- init.el
+++ init.el
@@ -13,9 +13,9 @@
 
 ;; My preferred fonts
 (when window-system
-  (set-face-attribute 'default nil        :family "Input Mono" :height 110)
-  (set-face-attribute 'fixed-pitch nil    :family "Input Mono" :height 110)
-  (set-face-attribute 'variable-pitch nil :family "PT Serif"   :height 1.2))
+  (set-face-attribute 'default nil        :family "Input Mono Narrow" :height 120)
+  (set-face-attribute 'fixed-pitch nil    :family "Input Mono Narrow" :height 120)
+  (set-face-attribute 'variable-pitch nil :family "PT Serif"          :height 1.2))
 
 ;; No startup splash screen
 (setq inhibit-startup-message t
