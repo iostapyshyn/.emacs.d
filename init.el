5677e1e2c98068eb698ab55d0e568154151bd822
Illia Ostapyshyn <ilya.ostapyshyn@gmail.com>
2021-05-04 00:02:53 +0200

    outdated comment

--- init.el
+++ init.el
@@ -471,7 +471,6 @@ If the input is empty, select the previous history element instead."
   (setq ivy-use-virtual-buffers nil)
   (setq ivy-use-selectable-prompt t)
   (setq enable-recursive-minibuffers t)
-  ;; M-x man causing some problems
   (add-to-list 'ivy-preferred-re-builders '(ivy--regex-or-literal . "none"))
   (setq ivy-re-builders-alist
         '((t . ivy--regex-plus))))
