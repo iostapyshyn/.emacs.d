6d889e957ff6a80dd1404e684e03f993c57dc84e
Illia Ostapyshyn <ilya.ostapyshyn@gmail.com>
2021-06-06 22:57:59 +0200

    org elpa is no more

--- init.el
+++ init.el
@@ -243,7 +243,6 @@ Transient Mark mode is on but the region is inactive."
 (eval-when-compile
   (require 'package)
   (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-  (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
   (package-initialize)
 
   (unless (package-installed-p 'use-package)
