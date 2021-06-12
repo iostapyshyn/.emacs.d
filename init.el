33173c8f9b50ac8e5130621b5e2e2f8bf96ce871
Illia Ostapyshyn <ilya.ostapyshyn@gmail.com>
2021-06-12 16:26:37 +0200

    epa pinentry

--- init.el
+++ init.el
@@ -31,6 +31,8 @@
 (prefer-coding-system 'utf-8)
 (setq-default buffer-file-coding-system 'utf-8-auto-unix)
 
+(setq epa-pinentry-mode 'loopback)
+
 ;; Error bell and y/n confirmation
 (setq visible-bell nil)
 (setq ring-bell-function 'ignore)
