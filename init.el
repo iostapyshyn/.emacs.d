1e1d4e0bc18ed7b9460ba6218c559805cd88bde7
Illia Ostapyshyn <ilya.ostapyshyn@gmail.com>
2021-07-02 09:50:11 +0200

    set fullscreen in default-frame-alist

--- init.el
+++ init.el
@@ -12,14 +12,13 @@
 ;; (load custom-file) ;; Customize is not used
 
 ;; Frame appearance
+(add-to-list 'default-frame-alist '(fullscreen . fullscreen))
 (unless (member window-system '(ns mac))
   (menu-bar-mode -1))
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 
 (when window-system
-  (set-frame-parameter nil 'fullscreen 'fullscreen)
-
   (set-face-attribute 'default        nil :family "Iosevka"    :height 120 :width 'expanded)
   (set-face-attribute 'fixed-pitch    nil :family "Iosevka"    :height 120 :width 'expanded)
   (set-face-attribute 'variable-pitch nil :family "Sans Serif" :height 1.1))
