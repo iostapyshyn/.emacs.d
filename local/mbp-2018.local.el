41bc42a68ecdab615140d9a1b4c026f6f61f28dd
Illia Ostapyshyn <ilya.ostapyshyn@gmail.com>
2021-09-25 16:03:44 +0200

    libgccjit path on mac

--- local/mbp-2018.local.el
+++ local/mbp-2018.local.el
@@ -1,3 +1,9 @@
+(setenv "LIBRARY_PATH"
+  (mapconcat 'identity
+    '("/usr/local/opt/gcc/lib/gcc/11"
+      "/usr/local/opt/libgccjit/lib/gcc/11"
+      "/usr/local/opt/gcc/lib/gcc/11/gcc/x86_64-apple-darwin20/11.2.0") ":"))
+
 (set-face-attribute 'default        nil :family "Iosevka Custom" :height 120)
 (set-face-attribute 'fixed-pitch    nil :family "Iosevka Custom" :height 120)
 (set-face-attribute 'variable-pitch nil :height 1.1)
