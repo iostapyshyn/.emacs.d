0d2dfb22b9f4fc5ce7251e8ea0417881a783bb57
Illia Ostapyshyn <ilya.ostapyshyn@gmail.com>
2021-11-19 11:10:13 +0100

    no fullscreen in local

--- local/mbp-2018.local.el
+++ local/mbp-2018.local.el
@@ -8,8 +8,6 @@
 (set-face-attribute 'fixed-pitch    nil :family "Iosevka Custom" :height 120)
 (set-face-attribute 'variable-pitch nil :height 1.1)
 
-(add-to-list 'default-frame-alist '(fullscreen . fullscreen))
-
 (setq mouse-wheel-scroll-amount '(1 ((shift) . hscroll)))
 (setq mouse-wheel-progressive-speed nil)
 (setq mouse-wheel-follow-mouse t)
