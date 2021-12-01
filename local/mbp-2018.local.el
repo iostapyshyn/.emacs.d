616e32843d609bb35ad418a3caa284056ce3e581
Illia Ostapyshyn <ilya.ostapyshyn@gmail.com>
2021-12-01 23:44:36 +0100

    local font

--- local/mbp-2018.local.el
+++ local/mbp-2018.local.el
@@ -4,8 +4,8 @@
       "/usr/local/opt/libgccjit/lib/gcc/11"
       "/usr/local/opt/gcc/lib/gcc/11/gcc/x86_64-apple-darwin21/11.2.0") ":"))
 
-(set-face-attribute 'default        nil :family "Iosevka Custom" :height 120)
-(set-face-attribute 'fixed-pitch    nil :family "Iosevka Custom" :height 120)
+(set-face-attribute 'default        nil :family "Iosevka Term" :width 'expanded :height 120)
+(set-face-attribute 'fixed-pitch    nil :family "Iosevka Term" :width 'expanded :height 120)
 (set-face-attribute 'variable-pitch nil :height 1.1)
 
 (setq mouse-wheel-scroll-amount '(1 ((shift) . hscroll)))
