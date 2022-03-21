a4328935d1f401a8661b72df4aea69ac53c26c80
Illia Ostapyshyn <ilya.ostapyshyn@gmail.com>
2022-03-21 15:32:33 +0100

    rust run clippy

--- init.el
+++ init.el
@@ -778,8 +778,7 @@ the buffer. Disable flyspell-mode otherwise."
 (use-package rust-mode
   :ensure t
   :config
-  (setq rust-format-on-save t)
-  (define-key rust-mode-map (kbd "C-c C-c") #'rust-run)
+  (define-key rust-mode-map (kbd "C-c C-c") #'rust-run-clippy)
   (add-hook 'rust-mode-hook
             (lambda () (setq indent-tabs-mode nil))))
 
