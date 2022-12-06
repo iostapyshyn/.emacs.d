640c465036b9bd23e7e1c70916d1f0ee28c10f93
Illia Ostapyshyn <ilya.ostapyshyn@gmail.com>
2022-12-06 13:16:12 +0100

    Update libgccjit version for mac

--- local/mbp-2018.local.el
+++ local/mbp-2018.local.el
@@ -1,8 +1,8 @@
 (setenv "LIBRARY_PATH"
   (mapconcat 'identity
-    '("/usr/local/opt/gcc/lib/gcc/11"
-      "/usr/local/opt/libgccjit/lib/gcc/11"
-      "/usr/local/opt/gcc/lib/gcc/11/gcc/x86_64-apple-darwin21/11") ":"))
+    '("/usr/local/opt/gcc/lib/gcc/12"
+      "/usr/local/opt/libgccjit/lib/gcc/12"
+      "/usr/local/opt/gcc/lib/gcc/12/gcc/x86_64-apple-darwin21/12") ":"))
 
 (set-face-attribute 'default        nil :family "Iosevka Term" :width 'expanded :height 120)
 (set-face-attribute 'fixed-pitch    nil :family "Iosevka Term" :width 'expanded :height 120)
