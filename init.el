7eb949b5ae97b3e7a9922f485c7319f7993c2b1e
Illia Ostapyshyn <ilya.ostapyshyn@gmail.com>
2023-06-11 20:21:30 +0200

    man: be pushy

--- init.el
+++ init.el
@@ -159,7 +159,7 @@ aligned respectively."
 (global-set-key (kbd "M-g i") #'imenu)
 
 (global-set-key (kbd "C-h M") #'man)
-(setq Man-notify-method 'thrifty)
+(setq Man-notify-method 'pushy)
 
 (defun toggle-window-dedicated (&optional window)
   "Invert the dedicatation of the WINDOW to its buffer."
