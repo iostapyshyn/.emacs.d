a7902be9a4b472349c80ae91368070048e6c6dcf
Illia Ostapyshyn <ilya.ostapyshyn@gmail.com>
2023-07-10 10:12:40 +0200

    Local template

--- local/template.el
+++ local/template.el
@@ -1,9 +1,3 @@
-(setenv "LIBRARY_PATH"
-  (mapconcat 'identity
-    '("/usr/local/opt/gcc/lib/gcc/13"
-      "/usr/local/opt/libgccjit/lib/gcc/13"
-      "/usr/local/opt/gcc/lib/gcc/13/gcc/x86_64-apple-darwin22/13") ":"))
-
 (set-face-attribute 'default        nil :family "Iosevka Term" :width 'expanded :height 120)
 (set-face-attribute 'fixed-pitch    nil :family "Iosevka Term" :width 'expanded :height 120)
 (set-face-attribute 'variable-pitch nil :height 1.1)
