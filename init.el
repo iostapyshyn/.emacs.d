8fdc321d0e8821b074941f620339555ba7d4c325
Illia Ostapyshyn <ostapyshyn@sra.uni-hannover.de>
2023-08-04 15:24:08 +0200

    Remove haskell from treesit-langs
    There's no haskell-ts-mode yet

--- init.el
+++ init.el
@@ -260,8 +260,7 @@ If point reaches the beginning or end of buffer, it stops there."
 (require 'treesit)
 
 (defconst treesit-langs
-  '(("c" . c) ("c++" . cpp) ("bash" . bash) ("python" . python) ("haskell" . haskell)
-    ("rust" . rust)))
+  '(("c" . c) ("c++" . cpp) ("bash" . bash) ("python" . python) ("rust" . rust)))
 
 (defun treesit-populate-mode-mapping ()
   "Populate `major-mode-remap-alist' according to `treesit-langs'."
