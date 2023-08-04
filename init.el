52811c9c586d654a421c82fc56d550c90865fb22
Illia Ostapyshyn <ilya.ostapyshyn@gmail.com>
2023-08-04 14:52:58 +0200

    Add consult-mark keybinding

--- init.el
+++ init.el
@@ -662,6 +662,7 @@ the buffer. Disable flyspell-mode otherwise."
   :bind (("M-s l"                                 . consult-line)
          ("M-s g"                                 . consult-git-grep)
          ("M-s r"                                 . consult-ripgrep)
+         ("M-g m"                                 . consult-mark)
          ([remap switch-to-buffer]                . consult-buffer)
          ([remap switch-to-buffer-other-window]   . consult-buffer-other-window)
          ([remap switch-to-buffer-other-frame]    . consult-buffer-other-frame)
